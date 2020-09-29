9c7a45eb51d5fb3aa93ec2ca9b5b2ac9cd5543a0
Sam <tritoke@protonmail.com>
2020-09-29 14:48:37 +0100

    Added OTP functionality.

--- one-time-pad.py
+++ one-time-pad.py
@@ -1,3 +1,40 @@
 #!/usr/bin/env python
+import random
+import binascii
 
-print("Hello World!")
+random.seed(42)
+
+def generate_pad(message):
+    pad = []
+    for i in range(len(message)):
+        pad.append(random.randint(0, 255))
+
+    return binascii.hexlify(bytes(pad))
+
+def encrypt(message, pad):
+    pad = binascii.unhexlify(pad)
+
+    encrypted = []
+    for m, p in zip(message, pad):
+        encrypted.append(m ^ p)
+
+    return binascii.hexlify(bytes(encrypted))
+
+def decrypt(encrypted, pad):
+    encrypted = binascii.unhexlify(encrypted)
+    
+    decrypted = encrypt(encrypted, pad)
+
+    return binascii.unhexlify(decrypted)
+
+message = b"Hi my name is Sam and this is a message!"
+
+pad = generate_pad(message)
+encrypted = encrypt(message, pad)
+
+decrypted = decrypt(encrypted, pad)
+
+print(f"Original message:  {message}")
+print(f"One Time Pad:      {pad}")
+print(f"Encrypted message: {encrypted}")
+print(f"Decrypted message: {decrypted}")
